8e57ce31466f3944da666a4ef37c49e70f1b33ed
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-09-16 21:17:42 +0300

    added img loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Post from './Post';
 import './styles/main.css';
 import json from './assets/json.json';
+import WebpackLogo from './assets/webpack.jpg';
 
-const post = new Post('Webpack post title');
+const post = new Post('Webpack post title', WebpackLogo);
 
 console.log('Post to string: ', post.toString());
 console.log(json);
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|giv|jpg)$/,
+        use: ['file-loader'],
+      },
     ],
   },
 };
