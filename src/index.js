b1a9614c99e7b6bee9473152f97ff0471a8f9028
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-09-16 21:00:15 +0300

    added json xml cvs ico files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Post from './Post';
 import './styles/main.css';
+import json from './assets/json.json';
 
 const post = new Post('Webpack post title');
 
 console.log('Post to string: ', post.toString());
+console.log(json);
