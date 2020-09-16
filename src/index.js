4c9eba63b185decc3ba6613df2a916443051793c
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-09-16 22:24:38 +0300

    add cvs loader

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import Post from './Post';
 import './styles/main.css';
 import json from './assets/json.json';
 import WebpackLogo from './assets/webpack.png';
+import XML from './assets/xml.xml';
+import CSV from './assets/csv.csv';
 
 const post = new Post('Webpack post title', WebpackLogo);
 
 console.log('Post to string: ', post.toString());
-console.log(json);
+console.log('JSON: ', json);
+console.log('XML: ', XML);
+console.log('CSV: ', CSV);
